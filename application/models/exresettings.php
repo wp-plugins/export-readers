b21db5f300ef818ba6a09e26e680e3b4eea80030
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-08 13:35:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/export-readers/trunk@357196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- application/models/exresettings.php
+++ application/models/exresettings.php
@@ -3,29 +3,6 @@ class exresettings extends wv19v_settings {
 	public function __construct($application)
 	{
 		parent::__construct($application);
-		$this->legacy_move('exreader');
+		$this->legacy_move('exreader','options');
 	}
-/*	public function prepare_value($values)
-	{
-		global $wp_roles;
-		$roles = $wp_roles->get_names();
-		$roles['commenter'] = 'Commenter';
-		$setObj = new ExReSettings();
-		foreach($values['roles'] as $key=>$value)
-		{
-			if(!in_array($key,$roles,true))
-			{
-				unset($values['roles'][$key]);
-			}			
-		}
-		foreach($roles as $key=>$value)
-		{
-			if(!isset($values['roles'][$value]))
-			{
-				$values['roles'][$value]='';
-			}			
-		}
-		ksort($values['roles']);	
-		return $values;
-	}
-*/}
+}

d4154464481b1f9840c3c0c469f13635a22461e1
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 17:13:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/export-readers/trunk@385141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- export-readers.php
+++ export-readers.php
@@ -1,11 +1,10 @@
 <?php /*
-Plugin Name: &nbsp;ExportReaders
+Plugin Name: ExportReaders
 Plugin URI: http://wordpress.org/extend/plugins/export-readers/
 Description: Selectively export all your reader details ( users and commenters )
 Author: dcoda
 Author URI: http://dcoda.co.uk
-Version: 1.2.0.27
+Version: 1.2.0.28
  */ 
 require_once  dirname ( __FILE__ ) . '/library/wordpress/application.php';
-
-new wv28v_application ( __FILE__,array('exrecomments','exreusers'),'exresettings' );
+$exportreaders = new wv28v_application ( __FILE__,array('exrecomments','exreusers'),'exresettings' );

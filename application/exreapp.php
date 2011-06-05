b445252fa4e336ce265469d474ecdcc76b9989c3
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-05 16:14:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/export-readers/trunk@393502 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- application/exreapp.php
+++ application/exreapp.php
@@ -3,9 +3,9 @@ if (! class_exists ( 'wv30v_application' )) :
 	require_once dirname ( dirname ( __FILE__ ) ) . '/library/wordpress/application.php';
 endif;
 class exreapp extends wv30v_application {
-	public function __construct($application)
+	public function __construct($file)
 	{
-		parent::__construct($application);
+		parent::__construct($file);
 		$this->legacy_move('exreader','options');
 	}
 }

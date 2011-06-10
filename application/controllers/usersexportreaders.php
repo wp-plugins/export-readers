7a25e1ae5990d14503439dddcae4a59a8c3e733e
dcoda <dcoda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-10 19:20:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/export-readers/trunk@395690 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- application/controllers/usersexportreaders.php
+++ application/controllers/usersexportreaders.php
@@ -63,7 +63,6 @@ class usersexportreaders extends wv30v_controller_action_wphooks {
 	{
 		$this->view->settings = $this->settings()->post('options');
 		$this->view->download_url=$this->control_url('readers.csv');
-		//return $content.$this->updated();
 	}
 	public function getting_startedActionMeta($return)
 	{
